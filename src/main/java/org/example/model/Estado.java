2789965c72de41bb2cb0e6e8f32b0fb49dd0681b
jesus <jesussantamariadiaz299@gmail.com>
2025-06-04 12:49:18 -0500

    proyecto inicializado

--- src/main/java/org/example/model/Estado.java
+++ src/main/java/org/example/model/Estado.java
@@ -1,13 +1,1 @@
-package org.example.model;
 
-public enum Estado {
-    PENDIENTE("Tarea pendiete"),
-    ENPROCESO("Tarea en proceso"),
-    COMPLETADA("Tarea completada");
-
-    private final String descripcion;
-
-    Estado(String descripcion) {
-        this.descripcion = descripcion;
-    }
-}
--- src/main/java/org/example/model/Prioridad.java
+++ src/main/java/org/example/model/Prioridad.java
@@ -1,13 +1,1 @@
-package org.example.model;
 
-public enum Prioridad {
-    INMEDIATO("Inmediato"),
-    IMPORTANTE("Importante"),
-    CONTIEMPO("Contiempo");
-
-    private final String descripcion;
-
-     Prioridad(String descripcion) {
-        this.descripcion = descripcion;
-    }
-}
